00f689e27281c64e4372bbababbca70455455921
Jaeho-99 <wogh6860@gmail.com>
2023-12-06 17:25:10 +0900

    fix: 데이터 날짜 범위 수정

--- data_load/main.py
+++ data_load/main.py
@@ -5,8 +5,10 @@ import load
 
 if __name__ == "__main__":
 
+    current_date_str = datetime.now().strftime('%Y-%m-%d')
     df_parmeters = pd.read_excel("parameter_values.xlsx")
-    choice = int(input("데이터 수집 종류 입력(1: 과거, 2: 당일)>> "))
+
+    choice = int(input("데이터 수집 종류 입력(1: 과거부터 금일까지, 2: 금일만)>> "))
     if choice == 1:
         csv_file_path = "dataset.csv"
         parser = parsing.Parser(csv_filename=csv_file_path)
@@ -18,7 +20,7 @@ if __name__ == "__main__":
                 'action': 'periodProductList',
                 'p_productclscode': '02',
                 'p_startday': '2023-01-01',
-                'p_endday': '2023-11-30',
+                'p_endday': current_date_str,
                 'p_itemcategorycode': f'{itemcategorycode}',
                 'p_itemcode': f'{itemcode}',
                 'p_kindcode': f'{kindcode:02d}',
@@ -32,7 +34,6 @@ if __name__ == "__main__":
         load.upload_to_s3(file_path = csv_file_path)
 
     elif choice == 2:
-        current_date_str = datetime.now().strftime('%Y-%m-%d')
         csv_file_path = f"dataset({current_date_str}).csv"
         parser = parsing.Parser(csv_filename = csv_file_path)
         for i in range(len(df_parmeters)):
